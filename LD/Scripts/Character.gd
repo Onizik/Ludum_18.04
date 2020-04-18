4cf73c9a58ad3f41f1fb357e75e00b1dd006e94c
Onizik <Onizik@mail.ru>
2020-04-18 15:51:45 +0600

    Add character

---
+++ LD/Scripts/Character.gd
@@ -0,0 +1,20 @@
+extends Node2D
+var speed = 400
+var screen_size
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+
+func _process(delta):
+	var direction = Vector2()
+	if Input.is_action_pressed("ui_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("ui_right"):
+		direction.x += 1
+
+	position += direction.normalized() * delta * speed
+	position.x = clamp(position.x, 0, screen_size.x)
+
