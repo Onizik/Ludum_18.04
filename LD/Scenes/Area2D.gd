6e979762917b456f33ac51288180b9e40e91995f
Onizik <Onizik@mail.ru>
2020-04-18 23:56:58 +0600

    add collision

---
+++ LD/Scenes/Area2D.gd
@@ -0,0 +1,22 @@
+extends Area2D
+signal hit
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Area2D_body_entered(body):
+	$test.visible = true
+	emit_signal("hit")
+	$CollisionShape2D.set_deferred("disabled", true)
