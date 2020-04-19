a960a476a3222eeb51f96525235ab39fdec4c00d
Onizik <Onizik@mail.ru>
2020-04-19 11:09:18 +0600

    create all levels

--- LD/Scripts/Character.gd
+++ LD/Scripts/Character.gd
@@ -16,7 +16,30 @@ func _process(delta):
 		direction.x += 1
 	if Input.is_action_pressed("ui_accept"):
 		if $Area2D/test.visible:
-			get_tree().change_scene("res://Scenes/Menu.tscn")
+			match $Area2D/doornumber.text:
+				"1":
+					get_tree().change_scene("res://Levels/first_stage/firstLVL.tscn")
+				"01":
+					get_tree().change_scene("res://Levels/main_stage/Main.tscn")
+				"2":
+					get_tree().change_scene("res://Levels/first_stage/secondLVL.tscn")
+				"3":
+					get_tree().change_scene("res://Levels/first_stage/thirdLVL.tscn")
+				"4":
+					get_tree().change_scene("res://Levels/first_stage/4LVL.tscn")
+				"5":
+					get_tree().change_scene("res://Levels/first_stage/5LVL.tscn")
+				"6":
+					get_tree().change_scene("res://Levels/second_stage/6LVL.tscn")
+				"7":
+					get_tree().change_scene("res://Levels/second_stage/7LVL.tscn")
+				"8":
+					get_tree().change_scene("res://Levels/second_stage/8LVL.tscn")
+				"9":
+					get_tree().change_scene("res://Levels/second_stage/9LVL.tscn")
+				"10":
+					get_tree().change_scene("res://Levels/second_stage/10LVL.tscn")
+
 
 	position += direction.normalized() * delta * speed
 	position.x = clamp(position.x, 0, screen_size.x)
