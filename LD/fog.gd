822111eaaf69fa85b4a653c196f5b0af5cb698f6
Onizik <Onizik@mail.ru>
2020-04-21 00:57:22 +0600

    add second stage

--- LD/fog.gd
+++ LD/fog.gd
@@ -8,10 +8,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$AnimationPlayer.play("2")
-	if Global.item2 == true:
-		$shadow.visible = false
-
+	$AnimationPlayer.play("4")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
