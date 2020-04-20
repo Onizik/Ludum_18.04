ae61803e377489c9bee260289c10952e18d58747
Onizik <Onizik@mail.ru>
2020-04-20 22:48:47 +0600

    add door anim

--- LD/In.gd
+++ LD/In.gd
@@ -8,10 +8,9 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
-func _process(delta):
-	if Global.loh :
-		$loh.visible = true
-	else: $loh.visible = false
-	
-	
+	$AnimationPlayer.play("in")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
