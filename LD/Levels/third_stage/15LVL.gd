9e870676abe6db327e3644b1868c36af290aaa19
Onizik <Onizik@mail.ru>
2020-04-20 14:31:34 +0600

    add third item

---
+++ LD/Levels/third_stage/15LVL.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _process(delta):
+	if Global.item3 :
+		$item3/CollisionShape2D.disabled = true
+		$item3.visible = false
